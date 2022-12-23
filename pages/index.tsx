4efc90d37d9ff6462d3231d85e1acb4f067849a1
joshanaba <anabajoshchi@gmail.com>
2022-12-23 12:57:47 +0100

    input can now accept changes in between, focus on next box etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.scss'
+import OTPInput from '../components/OTPInput'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [otp, setOtp] = useState('');
+  const onChange = (value: string) => setOtp(value);
   return (
     <>
       <Head>
@@ -16,7 +20,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h2>Next Typescript OTP Input</h2>
-
+        <OTPInput value={otp} valueLength={6} onChange={onChange} />
       </main>
     </>
   )
